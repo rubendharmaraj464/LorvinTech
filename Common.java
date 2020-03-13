467855ae4552957339c62c8f47594794f0aab8b0
rubeen Dharmaraj <60965476+rubendharmaraj464@users.noreply.github.com>
2020-03-13 06:38:28 +0530

    Add files via upload
    common file is added into master branch

---
+++ Common.java
@@ -0,0 +1,36 @@
+package demoPacakge;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Common {
+
+	public static void main(String[] args) throws Exception  {
+		System.setProperty("webdriver.chrome.driver","D:\\Ruben\\Lorvin Technologes\\chromedriver_win32\\chromedriver.exe"); 
+
+        WebDriver driver=new ChromeDriver(); 
+
+        driver.navigate().to("http://www.javatpoint.com/"); 
+
+  
+
+  //Maximize the browser 
+
+   driver.manage().window().maximize(); 
+
+   Thread.sleep(5000);
+
+   driver.findElement(By.id("gs_tti50")).click();
+
+	}
+	
+	public void Print()
+	{
+		System.out.print("Called common Succesfully");
+		//code
+		//code
+	}
+	
+
+}
